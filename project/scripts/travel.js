ea060ef47dabefc56c498e527a14c73ac5a0dfcc
Ir Benjamin <benjaminiriganje@gmail.com>
2026-06-11 19:36:53 +0200

    first commit

---
+++ project/scripts/travel.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+```
+const form = document.getElementById("contactForm");
+
+if(form){
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Thank you for contacting Travel Burundi!");
+    });
+}
+```
+
+});
